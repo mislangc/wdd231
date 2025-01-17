dd5adf7cd13ace2463a3c79c3781a15b2f168e47
mislangc <carlbpmislang@gmail.com>
2025-01-17 21:06:13 +0800

    17. Chamber directory page fix: contrast and unused declarations

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -36,7 +36,6 @@ async function displayData(url) {
 async function getData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
     return data;
 }
 
